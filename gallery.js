09be236b4940253beda858ea362eae6efd2fa990
unknown <dotarost@mail.ru>
2021-04-18 15:45:35 +0300

    1-5 made

--- gallery.js
+++ gallery.js
@@ -8,12 +8,14 @@ import files from "./gallery-items.js";
 // src элемента img.lightbox**image. Это необходимо для того, чтобы при следующем
 // открытии модального окна, пока грузится изображение, мы не видели предыдущее.
 
-const containerGallery = document.querySelector('.js-gallery');
+
 
 const refs = {
-  imgOverlay: document.querySelector('.lightbox__overlay'),
-  imgLightbox: document.querySelector('.lightbox'),
-  imgCloseLightbox: document.querySelector('.lightbox__button'),
+  containerGallery: document.querySelector('.js-gallery'),
+  overlay: document.querySelector('.lightbox__overlay'),
+  lightbox: document.querySelector('.js-lightbox'),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
+  imgModul: document.querySelector('.lightbox__image'), 
 }
 
 function createMarkUp(files) {
@@ -34,17 +36,57 @@ function createMarkUp(files) {
 }).join('');
 };
 
-containerGallery.insertAdjacentHTML('beforeend', createMarkUp(files));
+refs.containerGallery.insertAdjacentHTML('beforeend', createMarkUp(files));
 
-containerGallery.addEventListener('click', onCreateGalleryClick);
+refs.containerGallery.addEventListener('click', onCreateGalleryClick);
+
 function onCreateGalleryClick(ev) {
-  // if (!ev.target.classList.contains('gallery__image')) {
-  //   return;
-  // }
+  if (!ev.target.classList.contains('gallery__image')) {
+    return;
+  }
   ev.preventDefault();
   console.log(ev.target);
+  const el = ev.target;
+  onOpenLightbox(el);
+   window.addEventListener('keydown', onEscPress);
+  console.log(refs.lightbox);
 }
 
+// refs.lightbox.addEventListener('click', onOpenLightbox);
+function onOpenLightbox(el) {
+  refs.lightbox.classList.add('is-open');
+  refs.overlay.addEventListener('click', onOverlayClick);
+  refs.closeBtn.addEventListener('click', onCloseLightbox);
+  refs.imgModul.src = el.dataset.source;
+  refs.imgModul.alt = el.alt;
+  
+};
 
+function onCloseLightbox() {
+  
+  refs.lightbox.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscPress);
+  onClearSrc();
+};
 
+function onOverlayClick() {
+  console.log('click on overlay');
+  console.log(ev.currentTarget);
+  console.log(ev.target);
 
+  if (ev.target === ev.currentTarget) {
+    console.log('click in overlay');
+    onCloseLightbox();
+  }
+};
+
+function onEscPress(ev) {
+  if (ev.code === 'Escape') {
+    onCloseLightbox();
+  }
+}
+
+function onClearSrc() {
+   refs.imgModul.src = '';
+  refs.imgModul.alt = '';
+}
